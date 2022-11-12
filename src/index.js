e8132d88e84b11ab7a20646a1ba00aec6caeef97
felipegiacomoni <felipe00510@gmail.com>
2022-11-12 13:19:53 -0300

    including redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
 import App from "./App";
+import reducers from "./reducers";
+import reduxThunk from 'redux-thunk';
+import { Provider } from "react-redux";
+
+const store = createStore(
+    reducers,
+    compose(applyMiddleware(reduxThunk))
+)
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
